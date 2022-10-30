eac249e0b0d016412ec8087c620d0b49b6d3330d
SuryaMN <suryamnnitk@gmail.com>
2022-10-30 18:36:48 +0530

    Added ip token map authorization

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
-
+from auth_middleware import token_required 
 import requests
 import os
 import json
@@ -9,13 +9,17 @@ app = Flask(__name__, template_folder='.')
 # app.secret_key = 'thisisjustarandomstring'
 
 
+ip_token_map = {}
+
 @app.route('/inventory', methods=['POST', 'GET'])
+@token_required(ip_token_map)
 def index():
     if(request.method == 'GET'):
         
         # data = requests.get("http://inventory-service:8001/getProducts")
         data = requests.get("https://inventory-service-capstone.herokuapp.com/getProducts")
         print(data)
+        print(ip_token_map)
         data = data.json()
         return data
 
---
+++ auth_middleware.py
@@ -0,0 +1,35 @@
+from flask import request, abort
+
+
+
+def token_required(ip_token_map):
+    def decorator(api_caller):
+        def wrapper(*args, **kwargs):
+            
+            ip  = request.remote_addr
+            token = None
+            if "Authorization" in request.headers:
+                token = request.headers["Authorization"]
+                print(token)
+            
+            if not token:
+                return {
+                    "message": "Authentication Token is missing!",
+                    "data": None,
+                    "error": "Unauthorized"
+                }, 401
+
+            if token in ip_token_map and ip_token_map[token] != ip:
+                return {
+                    "message": "This is an attempt for XSS attack",
+                    "data": None,
+                    "error": "Unauthorized"
+                }, 401
+
+            elif token not in ip_token_map:
+                ip_token_map[token] = ip
+
+            return api_caller(*args, **kwargs)
+
+        return wrapper
+    return decorator
