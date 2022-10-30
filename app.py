325ce17ba001c7359a166abcaf6b85ef496503c7
SuryaMN <suryamnnitk@gmail.com>
2022-10-30 13:18:02 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,29 @@
+
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
+
+import requests
+import os
+import json
+
+app = Flask(__name__, template_folder='.')
+# app.secret_key = 'thisisjustarandomstring'
+
+
+@app.route('/inventory', methods=['POST', 'GET'])
+def index():
+    if(request.method == 'GET'):
+        
+        # data = requests.get("http://inventory-service:8001/getProducts")
+        data = requests.get("https://inventory-service-capstone.herokuapp.com/getProducts")
+        print(data)
+        data = data.json()
+        return data
+
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=8080,
+        host="0.0.0.0"
+    )
