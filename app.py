fa2534c784dc4648e2094e57dda00ee62a032474
SuryaMN <suryamnnitk@gmail.com>
2022-11-09 11:57:23 +0530

    Changed requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 
 from flask import Flask, request, flash, redirect, url_for, jsonify
 from auth_middleware import token_required
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 import requests
-import os
-import json
 
 app = Flask(__name__, template_folder='.')
 # app.secret_key = 'thisisjustarandomstring'
@@ -12,21 +10,34 @@ CORS(app)
 
 ip_token_map = {}
 
-@app.route('/inventory', methods=['POST', 'GET'])
+@app.route('/inventory', methods=['GET'])
+@cross_origin()
 @token_required(ip_token_map)
-def index():
-    if(request.method == 'GET'):
+def inventory():
         
-        # data = requests.get("http://inventory-service:8001/getProducts")
-        data = requests.get("https://inventory-service-capstone.herokuapp.com/getProducts")
-        data = data.json()
-        return data
+    data = requests.get("http://localhost:8001/getProducts")
+    # data = requests.get("https://inventory-service-capstone.herokuapp.com/getProducts")
+    data = data.json()
+    # print(data)
+    return jsonify(data)
+
+@app.route('/addProduct', methods=['POST'])
+@cross_origin()
+@token_required(ip_token_map)
+def addProduct():        
+
+    body = request.get_json()
+    print(body)
+    data = requests.post("http://localhost:8001/addProduct",json=body)
+    # data = requests.post("https://inventory-service-capstone.herokuapp.com/getProducts")
+    data = data.json()
+    # data = {'msg':"Data added successfully"}
+    return jsonify(data)
 
 
 
 if __name__ == '__main__':
     app.run(
         debug=True,
-        port=8080,
-        host="0.0.0.0"
+        port=8080
     )
--- auth_middleware.py
+++ auth_middleware.py
@@ -5,19 +5,20 @@ from device_detector import DeviceDetector
 def token_required(ip_token_map):
     def decorator(api_caller):
         def wrapper(*args, **kwargs):
-            ip  = request.environ["HTTP_X_FORWARDED_FOR"]
-            user_agent  = request.headers['User-Agent']
-            device = DeviceDetector(user_agent).parse()
-            print(device.is_bot())  
-            print(device.os_name())   
-            print(device.os_version())
-            print(device.engine())
-            print(device.device_brand())
-            print(device.device_model())       
-            print(device.device_type())
 
-            # ip  = request.remote_addr
-            print("IP : ", ip)
+            ip = request.remote_addr
+            # ip  = request.environ["HTTP_X_FORWARDED_FOR"]
+            # user_agent  = request.headers['User-Agent']
+            # device = DeviceDetector(user_agent).parse()
+            # print(device.is_bot())  
+            # print(device.os_name())   
+            # print(device.os_version())
+            # print(device.engine())
+            # print(device.device_brand())
+            # print(device.device_model())       
+            # print(device.device_type())
+
+            # print("IP : ", ip)
             # print("User-Agent : ", user_agent)
 
 
@@ -48,5 +49,6 @@ def token_required(ip_token_map):
 
             return api_caller(*args, **kwargs)
 
+        wrapper.__name__ = api_caller.__name__
         return wrapper
     return decorator
