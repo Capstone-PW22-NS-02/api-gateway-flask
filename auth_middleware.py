65d570d3532f1aa11232b92925a4e11a0794f43b
SuryaMN <suryamnnitk@gmail.com>
2022-11-10 12:54:24 +0530

    Changed auth_middleware

--- auth_middleware.py
+++ auth_middleware.py
@@ -9,26 +9,28 @@ def token_required(ip_token_map):
             # ip = request.remote_addr
             ip  = request.environ["HTTP_X_FORWARDED_FOR"]
             user_agent  = request.headers['User-Agent']
-            device = DeviceDetector(user_agent).parse()
-            print(device.is_bot())  
-            print(device.os_name())   
-            print(device.os_version())
-            print(device.engine())
-            print(device.device_brand())
-            print(device.device_model())
-            print(device.device_type())
+            # device = DeviceDetector(user_agent).parse()
+            # print(device.is_bot())  
+            # print(device.os_name())   
+            # print(device.os_version())
+            # print(device.engine())
+            # print(device.device_brand())
+            # print(device.device_model())       
+            # print(device.device_type())
 
-            print("IP : ", ip)
-            print("User-Agent : ", user_agent)
+            print("Line 1 => IP : ", ip)
+            print("Line 2 => User-Agent : ", user_agent)
 
 
             token = None
             if "Authorization" in request.headers:
                 token = request.headers["Authorization"]
-                print("Token : ",token)
+                print("Line 3 => Token : ",token)
+                print("Line 4 => Map : ",ip_token_map)
+
             
             if not token:
-                print("Map : ",ip_token_map)
+                print("Line 5 => Map : ",ip_token_map)
                 return {
                     "message": "Authentication Token is missing!",
                     "data": None,
@@ -36,7 +38,7 @@ def token_required(ip_token_map):
                 }, 401
 
             if token in ip_token_map and ip_token_map[token] != ip:
-                print("Map : ",ip_token_map)
+                print("Line 6 => Map : ",ip_token_map)
                 return {
                     "message": "This is an attempt for XSS attack",
                     "data": None,
@@ -45,7 +47,7 @@ def token_required(ip_token_map):
 
             elif token not in ip_token_map:
                 ip_token_map[token] = ip
-                print("Map : ",ip_token_map)
+                print("Line 7 => Map : ",ip_token_map)
 
             return api_caller(*args, **kwargs)
 
